a34dd470a0c861dbfdfc9aa81fb7d04c2f39223e
Do Om <doyom123@gmail.com>
2017-10-06 23:49:44 -0400

    Fixed compilation errors

--- MVP/MVP.cpp
+++ MVP/MVP.cpp
@@ -12,49 +12,52 @@ using namespace std;
 /* Preprocessor macro for defining a weighted constant */
 #define GRAVITY 9.8
 
-/* define our MVP logic class, declare plugin function */ 
-class BipedalMovement : public Plugin 
+/* define our MVP logic class, declare plugin function */
+class BipedalMovement : public Plugin
 {
-	
+public:
 	BipedalMovement() : Plugin("InMoov Bipedal Kinematics")
 	{
 		/* define that Body files will be implemented */
 		require("Body");
 	}
 
-	virtual bool initialize() 
+	virtual bool initialize()
 	{
 		ToolBar* TB = new ToolBar("InMoov Kinematics");
-		TB
-		->addButton("Walk")
-		->sigClicked()
-		.connect(bind(&MVP::onButtonClicked, this, +0.02)); 
+		TB->addButton("Walk")
+			->sigClicked()
+			.connect(bind(&BipedalMovement::onButtonClicked, this, +0.02));
 		/* From here, add buttons and functionality to the TB.
-		ToolBar class has member functions for adding buttons 
+		ToolBar class has member functions for adding buttons
 		and defining how the joint sliders react when clicked.
 		It is a self-bound function. Look around at the other
 		predefined functions for guidance. */
 
-		/* Note that this virtual function must return true. 
-		It may be a good idea to use this restriction as a 
+		/* Note that this virtual function must return true.
+		It may be a good idea to use this restriction as a
 		testing parameter */
+		addToolBar(TB);
 		return true;
 	}
 
-	 void onButtonClicked(double dq)
-    {
-	/* vector of type BodyItem */
-        ItemList<BodyItem> bodyItems =
-            ItemTreeView::mainInstance()->selectedItems<BodyItem>();
-        
-	/* size_t is defined as unsigned long long - to be used for large iterative values > 0 */
-	/* for loop: for each body item, assign a pointer to it, same with joints internal loop */
-        for(size_t i=0; i < bodyItems.size(); ++i){
-            BodyPtr body = bodyItems[i]->body();
-            for(int j=0; j < body->numJoints(); ++j){
-                body->joint(j)->q() += dq;
-            }
-            bodyItems[i]->notifyKinematicStateChange(true);
-        }
+	void onButtonClicked(double dq)
+	{
+		/* vector of type BodyItem */
+		ItemList<BodyItem> bodyItems =
+		    ItemTreeView::mainInstance()->selectedItems<BodyItem>();
+
+		/* size_t is defined as unsigned long long - to be used for large iterative values > 0 */
+		/* for loop: for each body item, assign a pointer to it, same with joints internal loop */
+		for(size_t i=0; i < bodyItems.size(); ++i){
+		    BodyPtr body = bodyItems[i]->body();
+		    for(int j=0; j < body->numJoints(); ++j){
+		        body->joint(j)->q() += dq;
+		    }
+		    bodyItems[i]->notifyKinematicStateChange(true);
+		}
+	}
 
 };
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(BipedalMovement)
